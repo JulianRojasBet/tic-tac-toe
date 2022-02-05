920514c70cb6f705c068468a84fa05324e7dced1
Julian Rojas <julianrojasbet@gmail.com>
2022-02-05 02:11:28 -0500

    Change private TURN server

--- src/lib/utils/webRTC.ts
+++ src/lib/utils/webRTC.ts
@@ -1,8 +1,8 @@
 const servers: RTCConfiguration = {
   iceServers: [
     {
-      urls: ['turn:172.31.92.8:443', 'stun:stun.l.google.com:19302'],
-      username: 'tic-tac-toe',
+      urls: ['turn:numb.viagenie.ca', 'stun:stun.l.google.com:19302'],
+      username: import.meta.env.VITE_TURN_USERNAME as string,
       credential: import.meta.env.VITE_TURN_PASSWORD as string,
       credentialType: 'password'
     },
